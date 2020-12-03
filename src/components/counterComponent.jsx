db5e57758e03f9a6694b5d9d9b7f70aca9631f4c
pnguye11 <pnguye11@gmail.com>
2020-12-03 11:11:59 -0800

    rendering classes dynamically

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,18 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200",
+    count: 1,
+    // imageUrl: "https://picsum.photos/200",
+  };
+  styles = {
+    fontSize: 30,
+    fontWeight: "bold",
   };
   render() {
+    let classes = this.getBadgeClasses();
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "zero" : count;
